e30563b2b67eb7e6dab5d0ae91f78dc6899fc0a1
Alexey Korshun <lehakorshun@gmail.com>
2018-09-28 13:33:23 +0600

    init pagination example

---
+++ pagination/src/main/java/com/rosberry/pagination/MoviesActivity.kt
@@ -0,0 +1,12 @@
+package com.rosberry.pagination
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MoviesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies)
+    }
+}
