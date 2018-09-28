ae1a1b36f7e1ccbb95cd4a97db37e51dbc47a033
neestell <neestell@gmail.com>
2018-09-28 14:09:25 +0600

    Changed strategy type for set posts

--- app/src/main/kotlin/com/rosberry/android/sample/presentation/main/MainView.kt
+++ app/src/main/kotlin/com/rosberry/android/sample/presentation/main/MainView.kt
@@ -1,12 +1,16 @@
 package com.rosberry.android.sample.presentation.main
 
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.rosberry.android.sample.presentation.main.list.PostItem
 import com.rosberry.android.sample.ui.base.model.DialogModel
 import com.rosberry.android.sample.ui.main.list.PostsAdapter
 
 interface MainView : MvpView, PostsAdapter.OnItemClickListener {
     fun setTitle(text: String)
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
     fun setPosts(posts: List<PostItem>)
 
     fun showProgress(visible: Boolean)
