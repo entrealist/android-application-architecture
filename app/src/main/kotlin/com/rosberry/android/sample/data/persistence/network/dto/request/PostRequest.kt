4082dc2dbfd04d1656eac34741ac35834e8e6362
neestell <neestell@gmail.com>
2018-09-28 13:35:50 +0600

    Fix code style again

--- app/src/main/kotlin/com/rosberry/android/sample/data/persistence/network/dto/request/PostRequest.kt
+++ app/src/main/kotlin/com/rosberry/android/sample/data/persistence/network/dto/request/PostRequest.kt
@@ -1,8 +1,9 @@
 package com.rosberry.android.sample.data.persistence.network.dto.request
 
-class PostRequest(val userId: Int,
-                  val title: String,
-                  val description: String
+class PostRequest(
+        val userId: Int,
+        val title: String,
+        val description: String
 ) : BaseRequest() {
 
 }
