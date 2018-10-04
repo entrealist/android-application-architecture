3e1bedeec05d8b84d8cff12cd4a248b010cb68e8
neestell <neestell@gmail.com>
2018-10-04 14:01:28 +0600

    Removed braces

--- app/src/main/kotlin/com/rosberry/android/sample/ui/base/model/PermissionModel.kt
+++ app/src/main/kotlin/com/rosberry/android/sample/ui/base/model/PermissionModel.kt
@@ -5,6 +5,4 @@ import android.support.annotation.StringRes
 /**
  * Created by dmitry on 08.02.2018.
  */
-class PermissionModel constructor(val name: String, @StringRes val noGrantPermissionId: Int) {
-
-}
+class PermissionModel constructor(val name: String, @StringRes val noGrantPermissionId: Int)
