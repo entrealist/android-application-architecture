74dd8bc0b0f7fe0677ec0bdf902397d20f5472a3
mmikhailov <netherpyro@gmail.com>
2018-11-13 11:22:46 +0600

    added user query methods; adjust other entity methods

--- roomdatabase/src/main/java/com/rosberry/sample/roomdatabase/db/Comment.kt
+++ roomdatabase/src/main/java/com/rosberry/sample/roomdatabase/db/Comment.kt
@@ -55,7 +55,7 @@ interface CommentDao {
     @get:Query("SELECT comment.id, article.title, user.firstName, user.lastName, user.email, comment.postedAt FROM comment INNER JOIN article ON comment.article_id = article.id INNER JOIN user ON comment.user_id = user.id ")
     val allCommentsAsSearchModel: Flowable<List<CommentSearchModel>>
 
-    @Query("SELECT comment.id, article.title AS title, user.firstName AS firstName, user.lastName AS lastName, user.email AS email, comment.postedAt FROM article INNER JOIN comment ON comment.article_id = article.id INNER JOIN user on user.id = comment.user_id WHERE user.email LIKE :userEmail AND comment.postedAt > :after ")
+    @Query("SELECT comment.id, article.title AS title, user.firstName AS firstName, user.lastName AS lastName, user.email AS email, comment.postedAt FROM article INNER JOIN comment ON comment.article_id = article.id INNER JOIN user ON user.id = comment.user_id WHERE user.email LIKE :userEmail AND comment.postedAt > :after ")
     fun findCommentsByUserNameAfterDate(userEmail: String, after: Instant): List<CommentSearchModel>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -68,8 +68,8 @@ interface CommentDao {
     fun updateComment(comment: Comment)
 
     @Delete
-    fun delete(comment: Comment)
+    fun delete(comment: Comment): Int
 
     @Query("DELETE FROM comment")
-    fun nukeComments()
+    fun nukeComments(): Int
 }
--- roomdatabase/src/main/java/com/rosberry/sample/roomdatabase/db/User.kt
+++ roomdatabase/src/main/java/com/rosberry/sample/roomdatabase/db/User.kt
@@ -6,9 +6,13 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.Ignore
 import android.arch.persistence.room.Index
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.OnConflictStrategy.IGNORE
 import android.arch.persistence.room.PrimaryKey
 import android.arch.persistence.room.Query
+import android.arch.persistence.room.Update
 import android.graphics.Bitmap
+import io.reactivex.Flowable
 import org.threeten.bp.LocalDate
 
 /**
@@ -28,8 +32,8 @@ data class User(
         var commentsPosted: Long = 0L,
         @Ignore var picture: Bitmap?
 ) {
-    // secondary constructor is needed due to Room cannot match ignored nullable parameter with field
-    // setting the parameter null does not affect
+    // Secondary constructor is needed due to Room cannot match ignored nullable parameter with field.
+    // Setting the parameter null does not affect.
     constructor(
             id: Long,
             email: String,
@@ -43,12 +47,33 @@ data class User(
 @Dao
 interface UserDao {
 
+    @get:Query("SELECT * FROM user")
+    val allUsers: Flowable<List<User>>
+
+    @Query("SELECT * FROM user WHERE firstName = :firstName AND lastName = :lastName")
+    fun findUserByFullName(firstName: String, lastName: String): List<User>
+
+    @Query("SELECT user.* FROM user INNER JOIN comment ON comment.user_id = user.id INNER JOIN article ON article.id = comment.article_id WHERE article.id LIKE :articleId")
+    fun findUsersCommentedArticle(articleId: Long): List<User>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(user: User): Long
+
+    @Insert(onConflict = IGNORE)
+    fun insertOrReplace(vararg users: User): List<Long>
+
     @Insert
-    fun insertAll(vararg users: User)
+    fun insertAll(vararg users: User): List<Long>
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun updateComment(user: User)
 
     @Delete
-    fun delete(user: User)
+    fun delete(user: User): Int
+
+    @Query("DELETE FROM user WHERE firstName LIKE :badName OR lastName LIKE :badName")
+    fun deleteUsersByName(badName: String): Int
 
     @Query("DELETE FROM user")
-    fun nukeUsers()
+    fun nukeUsers(): Int
 }
