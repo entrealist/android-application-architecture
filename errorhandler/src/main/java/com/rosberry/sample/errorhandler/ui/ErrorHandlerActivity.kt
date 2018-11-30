5610ab97bc3a9409e8b8c4087d854a014646936d
AlexeyKorshun <lehakorshun@gmail.com>
2018-11-30 13:40:35 +0700

    AND-71 (#12)

---
+++ errorhandler/src/main/java/com/rosberry/sample/errorhandler/ui/ErrorHandlerActivity.kt
@@ -0,0 +1,54 @@
+package com.rosberry.sample.errorhandler.ui
+
+import android.os.Bundle
+import com.arellomobile.mvp.MvpAppCompatActivity
+import com.arellomobile.mvp.presenter.InjectPresenter
+import com.arellomobile.mvp.presenter.ProvidePresenter
+import com.rosberry.sample.errorhandler.R
+import com.rosberry.sample.errorhandler.di.Component
+import com.rosberry.sample.errorhandler.entity.State
+import com.rosberry.sample.errorhandler.presentation.ErrorPresenter
+import com.rosberry.sample.errorhandler.presentation.ErrorView
+import kotlinx.android.synthetic.main.a_error_handler.*
+
+/**
+ * @author Alexei Korshun on 12/10/2018.
+ */
+class ErrorHandlerActivity : MvpAppCompatActivity(), ErrorView {
+
+    @InjectPresenter
+    lateinit var present: ErrorPresenter
+
+    @ProvidePresenter
+    fun providePresenter(): ErrorPresenter {
+        return Component(applicationContext)
+            .providePresenter()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.a_error_handler)
+        applyButton.setOnClickListener { present.proceed(processSelection(errorGroup.checkedRadioButtonId)) }
+        errorGroup.setOnCheckedChangeListener { _, checkedId -> processSelection(checkedId) }
+    }
+
+    override fun showError(title: String, description: String) {
+        titleView.text = title
+        descriptionView.text = description
+    }
+
+    override fun showResult(result: String) {
+        titleView.text = result
+        descriptionView.text = ""
+    }
+
+    private fun processSelection(id: Int): State {
+        return when (id) {
+            R.id.illegalStateExceptionCase -> State.ILLEGAL
+            R.id.unknownHostExceptionCase -> State.UNKNOWN_HOST
+            R.id.networkErrorExceptionCase -> State.NETWORK_ERROR
+            R.id.resultCase -> State.SUCCESS
+            else -> State.UNKNOWN
+        }
+    }
+}
