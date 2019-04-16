443d1f5962e0d138f946282230167dfa49cde00c
mmikhailov <netherpyro@gmail.com>
2019-04-16 10:12:51 +0600

    setup module

---
+++ workmanager/src/main/kotlin/com/rosberry/sample/workmanager/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rosberry.sample.workmanager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
