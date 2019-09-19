bd70deb2ec8dfcb7d48fa5cc661e57219641df90
neestell <neestell@gmail.com>
2019-09-19 17:34:08 +0600

    Add test view to check kts performance

--- app/src/main/kotlin/com/rosberry/android/sample/ui/MainActivity.kt
+++ app/src/main/kotlin/com/rosberry/android/sample/ui/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.rosberry.android.sample.ui
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
 import com.rosberry.android.sample.R
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,4 +15,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.a_main)
     }
 
+    fun onShowDatePicker(view: View) {
+        val calendar = Calendar.getInstance()
+        val year = calendar[Calendar.YEAR]
+        val month = calendar[Calendar.MONTH]
+        val day = calendar[Calendar.DAY_OF_MONTH]
+
+        DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+        }, year, month, day).show()
+    }
 }
